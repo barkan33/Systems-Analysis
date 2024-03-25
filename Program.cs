78ece3b00eeac5bf924f051af3b219f19f8e61fc
barkan33 <mishakolomyza@gmail.com>
2024-03-25 19:08:09 +0200

    2

--- Program.cs
+++ Program.cs
@@ -8,6 +8,32 @@
         {
             Console.WriteLine($"User: {connectedUser}            Dive Club: {diveClub}            Dive-Partners: {divePartners}");
             Console.WriteLine("Welcome To ProDrive 3000\n========================");
+            Console.WriteLine("Select:\n1.Login\n2.Register0.Exit");
+            int choice;
+            while (int.TryParse(Console.ReadLine(), out choice))
+            {
+                Console.WriteLine("Invalid Input");
+            }
+            switch (choice)
+            {
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                case 1:
+                    LogIn();
+                    break;
+                case 2:
+                    Register();
+                    break;
+
+            }
+        }
+        static public void LogIn()
+        {
+
+        }
+        static public void Register()
+        {
 
         }
     }

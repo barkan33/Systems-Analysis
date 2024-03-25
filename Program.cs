0c9b5fc982e04dfe9cd62c56bfe92b4fc9d0a607
barkan33 <mishakolomyza@gmail.com>
2024-03-25 18:35:43 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Systems_Analysis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

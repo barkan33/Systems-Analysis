eb2d8c3502e3f418d317292205528c38ce650d3a
Sergey Birarov <sergbirarov@gmail.com>
2024-03-28 11:41:30 +0200

    55

---
+++ System Analysis - Restaurant/SystemsAnalysis_Restaurant/Chef.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemsAnalysis_Restaurant
+{
+    internal class Chef : Staff
+    {
+        public Chef(string _username, string _password) : base(_username, _password) { }
+        public void PrepareFood() { }
+        public void UpdateOrderStatus() { }
+    }
+}
---
+++ System Analysis - Restaurant/SystemsAnalysis_Restaurant/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemsAnalysis_Restaurant
+{
+    internal class Customer
+    {
+        public void BrowseMenu()
+        {
+
+        }
+        public void SelectTable()
+        {
+
+        }
+        public void OrderFood()
+        {
+
+        }
+        public void Pay()
+        {
+            
+        }
+    }
+}
